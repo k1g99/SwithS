f0d9e7400adc6b5a91c549db19b92ffc04f9dbc9
k1g99 <idmatthew9546@gmail.com>
2023-10-11 01:48:14 +0900

    init: initial react project settings

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+}
---
+++ frontend/.prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  trailingComma: 'es5', // 객체나 배열 등 마지막 줄의 콤마 여부
+  tabWidth: 2, // 탭너비 2로 설정
+  semi: false, // 세미콜론 사용 여부
+  singleQuote: true, // 작은 따옴표 사용 여부
+}

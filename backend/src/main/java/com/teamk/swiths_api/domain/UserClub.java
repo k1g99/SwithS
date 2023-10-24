fe38456491b45191461b0a3b6f2d34c544e5afd2
k1g99 <idmatthew9546@gmail.com>
2023-10-24 23:42:52 +0900

    feat: create comment, userclub domain

---
+++ backend/src/main/java/com/teamk/swiths_api/domain/UserClub.java
@@ -0,0 +1,31 @@
+package com.teamk.swiths_api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserClub {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "club_id", nullable = false)
+    private Club club;
+
+}

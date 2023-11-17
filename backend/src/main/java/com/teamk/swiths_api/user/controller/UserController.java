4149b4a8888fcd796487fb8c8c3cd97cfbc05661
ShinKyungJune <rudwns2832@naver.com>
2023-11-17 16:04:34 +0900

    이메일 회원가입 구현중

--- backend/src/main/java/com/teamk/swiths_api/user/controller/UserController.java
+++ backend/src/main/java/com/teamk/swiths_api/user/controller/UserController.java
@@ -2,9 +2,11 @@ package com.teamk.swiths_api.user.controller;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,18 @@ import com.teamk.swiths_api.user.controller.dto.CreateUser.CreateUserRequest;
 import com.teamk.swiths_api.user.controller.dto.CreateUser.CreateUserResponse;
 import com.teamk.swiths_api.user.service.UserService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
+    
 
     // TODO: 차후, user 정보 필요할 시 만들기
     // @GetMapping("")
@@ -36,4 +42,6 @@ public class UserController {
 
         return result;
     }
+
+
 }
---
+++ backend/src/main/java/com/teamk/swiths_api/user/service/MailService.java
@@ -0,0 +1,39 @@
+package com.teamk.swiths_api.user.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender emailSender;
+
+    public void sendEmail(String toEmail, String title, String text) {
+        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+        try {
+            emailSender.send(emailForm);
+        } catch (RuntimeException e) {
+            // throw error
+            log.error("Send email error", e);
+        }
+    }
+
+    private SimpleMailMessage createEmailForm(String toEmail,
+            String title,
+            String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(title);
+        message.setText(text);
+
+        return message;
+    }
+}

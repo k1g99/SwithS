9dd6cac1203798c7898e6d5c44fb93c8cb17f604
ShinKyungJune <rudwns2832@naver.com>
2023-12-08 17:44:05 +0900

    토큰 통해 유저인포 반환

--- backend/src/main/java/com/teamk/swiths_api/user/jwt/SecurityUtil.java
+++ backend/src/main/java/com/teamk/swiths_api/user/jwt/SecurityUtil.java
@@ -2,6 +2,9 @@ package com.teamk.swiths_api.user.jwt;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.teamk.swiths_api.user.repository.entity.UserEntity;
 
 public class SecurityUtil {
     public static String getNowUserName() {
@@ -10,5 +13,14 @@ public class SecurityUtil {
             throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
         }
         return authentication2.getName();
+    }
+
+    public static String getNowUserId() {
+        final Authentication authentication2 = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication2 == null || authentication2.getName() == null) {
+            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
+        }
+
+        return authentication2.getPrincipal().toString();
     }
 }
